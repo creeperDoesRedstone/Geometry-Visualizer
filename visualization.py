4bc49c1d2778fe6681de629914bcd8945fa25ca0
creeperDoesRedstone <142895378+creeperDoesRedstone@users.noreply.github.com>
2024-08-12 20:32:21 +0700

    Update visualization.py
    Includes cubic Bezier curves and the ability to load from a text file instead of coding the elements directly.

--- visualization.py
+++ visualization.py
@@ -27,12 +27,13 @@ class Group:
     def add(self, sprite:Sprite): 
         self.sprites = self.sprites + [sprite]
     def get_sprite_type(self, sprite:Sprite):
-        if isinstance(sprite, BGText):          return -1
-        if isinstance(sprite, Line):            return 0
-        if isinstance(sprite, Triangle):        return 0
-        if isinstance(sprite, QuadBezierCurve): return 0
-        if isinstance(sprite, Point):           return 1
-        if isinstance(sprite, Text):            return 2
+        if isinstance(sprite, BGText):           return -1
+        if isinstance(sprite, Line):             return 0
+        if isinstance(sprite, Triangle):         return 0
+        if isinstance(sprite, QuadBezierCurve):  return 0
+        if isinstance(sprite, CubicBezierCurve): return 0
+        if isinstance(sprite, Point):            return 1
+        if isinstance(sprite, Text):             return 2
     def draw(self, surface:pygame.Surface):
         sorted_sprites = self.sprites
         sorted_sprites.sort(key=self.get_sprite_type)
@@ -41,6 +42,105 @@ class Group:
     def update(self):
         for sprite in self.sprites:
             sprite.update()
+
+def load(fn:str):
+    try:
+        file = open(fn, "r")
+    except FileNotFoundError:
+        raise Exception(f"File {fn} not found.")
+    else:
+        groups = []
+        for line in file:
+            if line != "\n":
+                line = line.replace("\n", "")
+                if line[0] == "@":
+                    group = Group()
+                    continue
+                elif line == "END":
+                    groups += [group]
+                    continue
+                spl_line = line.split()
+                if spl_line[0] == "BGTXT":
+                    spl_line[1] = spl_line[1].replace("`", " ").replace("Bezier", "Bézier")
+                    col = spl_line[2]
+                    if col[0] == "(" and col[-1] == ")":
+                        col = list(map(int, col[1:-1].split(",")))
+                    pos = spl_line[3]
+                    if pos[0] == "(" and pos[-1] == ")":
+                        pos = list(map(int, pos[1:-1].split(",")))
+                    elif pos.startswith("ADDR"):
+                        pos = group.sprites(int(pos[4:]))
+                    group.add(BGText(spl_line[1], col, pos, int(spl_line[4])))
+                elif spl_line[0] == "POINT":
+                    col = spl_line[4]
+                    if col[0] == "(" and col[-1] == ")":
+                        col = list(map(int, col[1:-1].split(",")))
+                    group.add(Point(int(spl_line[1]), int(spl_line[2]), int(spl_line[3]), col))
+                    if len(spl_line) == 6:
+                        if spl_line[5] == "False":
+                            group.sprites[-1].draggable = False
+                elif spl_line[0] == "LINE":
+                    spl_line[1] = group.sprites[int(spl_line[1][4:])]
+                    spl_line[2] = group.sprites[int(spl_line[2][4:])]
+                    group.add(Line(spl_line[1], spl_line[2], int(spl_line[3]), spl_line[4]))
+                elif spl_line[0] == "TRI":
+                    spl_line[1] = group.sprites[int(spl_line[1][4:])]
+                    spl_line[2] = group.sprites[int(spl_line[2][4:])]
+                    spl_line[3] = group.sprites[int(spl_line[3][4:])]
+                    if len(spl_line) == 5:
+                        group.add(Triangle(spl_line[1], spl_line[2], spl_line[3], int(spl_line[4])))
+                    if len(spl_line) == 6:
+                        group.add(Triangle(spl_line[1], spl_line[2], spl_line[3], int(spl_line[4]), spl_line[5]))
+                    if len(spl_line) == 7:
+                        group.add(Triangle(spl_line[1], spl_line[2], spl_line[3], int(spl_line[4]), spl_line[5], spl_line[6]))
+                elif spl_line[0] == "BCURVEQ":
+                    spl_line[1] = group.sprites[int(spl_line[1][4:])]
+                    spl_line[2] = group.sprites[int(spl_line[2][4:])]
+                    spl_line[3] = group.sprites[int(spl_line[3][4:])]
+                    col = spl_line[4]
+                    if col[0] == "(" and col[-1] == ")":
+                        col = list(map(int, col[1:-1].split(",")))
+                    group.add(QuadBezierCurve(spl_line[1], spl_line[2], spl_line[3], col, int(spl_line[5])))
+                elif spl_line[0] == "BCURVEC":
+                    spl_line[1] = group.sprites[int(spl_line[1][4:])]
+                    spl_line[2] = group.sprites[int(spl_line[2][4:])]
+                    spl_line[3] = group.sprites[int(spl_line[3][4:])]
+                    spl_line[4] = group.sprites[int(spl_line[4][4:])]
+                    col = spl_line[5]
+                    if col[0] == "(" and col[-1] == ")":
+                        col = list(map(int, col[1:-1].split(",")))
+                    group.add(CubicBezierCurve(spl_line[1], spl_line[2], spl_line[3], spl_line[4], col, int(spl_line[6])))
+                elif spl_line[0] == "TEXT":
+                    spl_line[1] = spl_line[1] = spl_line[1].replace("`", " ").replace("Bezier", "Bézier")
+                    col = spl_line[2]
+                    if col[0] == "(" and col[-1] == ")":
+                        col = list(map(int, col[1:-1].split(",")))
+                    pos = spl_line[3]
+                    if pos[0] == "(" and pos[-1] == ")":
+                        pos = list(map(int, pos[1:-1].split(",")))
+                    elif pos.startswith("ADDR"):
+                        pos = group.sprites[int(pos[4:])]
+                    group.add(Text(spl_line[1], col, pos, int(spl_line[4])))
+                elif spl_line[0] == "LNTXT":
+                    spl_line[1] = group.sprites[int(spl_line[1][4:])]
+                    spl_line[2] = group.sprites[int(spl_line[2][4:])]
+                    col = spl_line[3]
+                    if col[0] == "(" and col[-1] == ")":
+                        col = list(map(int, col[1:-1].split(",")))
+                    size = int(spl_line[4])
+                    if len(spl_line) > 5:
+                        offset = int(spl_line[5])
+                        if len(spl_line) > 6:
+                            spl_line[6] = spl_line[6].replace("`", " ").replace("Bezier", "Bézier")
+                            group.add(LineText(spl_line[1], spl_line[2], col, size, offset, spl_line[6]))
+                        else:
+                            group.add(LineText(spl_line[1], spl_line[2], col, size, offset))
+                    else:
+                        group.add(LineText(spl_line[1], spl_line[2], col, size))
+        else:
+            return groups
+    finally:
+        file.close()
 
 class Point(Sprite):
     def __init__(self, x, y, radius, col, draggable=True):
@@ -57,7 +157,7 @@ class Point(Sprite):
         mouse_down = pygame.mouse.get_pressed(3)[0]
         global dragging
         if mouse_down:
-            if pos[0] < self.x + self.radius and pos[0] > self.x - self.radius and pos[1] < self.y + self.radius and pos[1] > self.y - self.radius and not dragging:
+            if pos[0] < self.x + self.radius and pos[0] > self.x - self.radius and pos[1] < self.y + self.radius and pos[1] > self.y - self.radius and not dragging and self.draggable:
                 dragging = True
                 self.dragged = True
         else:
@@ -129,16 +229,48 @@ class QuadBezierCurve(Sprite):
         return Point(start_point.x + offset.x,
                      start_point.y + offset.y,
                      0, bg_col, False)
-    def bezier_interpolation(self, t:float):
-        inter_a = self.linear_interpolation(self.start_point, self.control_point, t)
-        inter_b = self.linear_interpolation(self.control_point, self.end_point, t)
-        return self.linear_interpolation(inter_a, inter_b, t)
     def draw(self, surface:pygame.Surface):
         res = 50
         previous_point = self.start_point
         for i in range(res):
             t = (i + 1) / res
-            next_point = self.bezier_interpolation(t)
+            inter_a = self.linear_interpolation(self.start_point, self.control_point, t)
+            inter_b = self.linear_interpolation(self.control_point, self.end_point, t)
+            next_point = self.linear_interpolation(inter_a, inter_b, t)
+            pygame.draw.line(surface, self.col,
+                             (previous_point.x, previous_point.y),
+                             (next_point.x, next_point.y),
+                             self.width
+                            )
+            previous_point = next_point
+
+class CubicBezierCurve(Sprite):
+    def __init__(self, p0:Point, p1:Point, p2:Point, p3:Point, col:pygame.Color, width:int):
+        self.p0 = p0
+        self.p1 = p1
+        self.p2 = p2
+        self.p3 = p3
+        self.col = col
+        self.width = width
+    def linear_interpolation(self, p0:Point, p1:Point, t:float) -> Point:
+        offset = Point((p1.x - p0.x) * t,
+                       (p1.y - p0.y) * t,
+                       0, bg_col, False)
+        return Point(p0.x + offset.x,
+                     p0.y + offset.y,
+                     0, bg_col, False)
+    def quadratic_interpolation(self, p0:Point, p1:Point, p2:Point, t:float) -> Point:
+        inter_a = self.linear_interpolation(p0, p1, t)
+        inter_b = self.linear_interpolation(p1, p2, t)
+        return self.linear_interpolation(inter_a, inter_b, t)
+    def draw(self, surface:pygame.Surface):
+        res = 50
+        previous_point = self.p0
+        for i in range(res):
+            t = (i + 1) / res
+            inter_a = self.quadratic_interpolation(self.p0, self.p1, self.p2, t)
+            inter_b = self.quadratic_interpolation(self.p1, self.p2, self.p3, t)
+            next_point = self.linear_interpolation(inter_a, inter_b, t)
             pygame.draw.line(surface, self.col,
                              (previous_point.x, previous_point.y),
                              (next_point.x, next_point.y),
@@ -155,7 +287,11 @@ class Text(Sprite):
     def draw(self, surface:pygame.Surface):
         text = self.font.render(self.content, True, self.col)
         text_rect = text.get_rect()
-        text_rect.center = (self.point.x, self.point.y - self.point.radius - 15) if isinstance(self.point, Point) else self.point
+        # text_rect.center = (self.point.x, self.point.y - self.point.radius - 15) if isinstance(self.point, Point) else self.point
+        if isinstance(self.point, Point):
+            text_rect.center = (self.point.x, self.point.y - self.point.radius - 15)
+        else:
+            text_rect.center = (self.point[0], self.point[1])
         surface.blit(text, text_rect)
 
 class BGText(Text):
@@ -183,78 +319,13 @@ class LineText(Text):
         text_rect.center = (self.point.x, self.point.y - self.point.radius - 15) if isinstance(self.point, Point) else self.point
         surface.blit(text, text_rect)
 
-group1 = Group()
-group2 = Group()
-group3 = Group()
-
-##########################
-# GROUP 1
-##########################
-
-# Background
-group1.add(BGText("Two points on a line", (120, 120, 120), (screen_width/2, 50), 32))
-group1.add(BGText("The red point is not draggable", (50, 50, 50), (screen_width/2, 500), 32))
-# Points
-group1.add(Point(200, 320, 10, "red", False))
-group1.add(Point(760, 320, 10, "white"))
-# Lines
-group1.add(Line(group1.sprites[2], group1.sprites[3], 3, "green"))
-# Text
-group1.add(Text("A", "white", group1.sprites[2], 20))
-group1.add(Text("B", "white", group1.sprites[3], 20))
-group1.add(LineText(group1.sprites[2], group1.sprites[3], "cyan", 20))
-group1.add(LineText(group1.sprites[2], group1.sprites[3], (0, 150, 150), 16, 40, "Length of line (px):"))
-
-##########################
-# GROUP 2
-##########################
-
-# Background
-group2.add(BGText("Triangle", (120, 120, 120), (screen_width/2, 50), 32))
-# Points
-group2.add(Point(330, 440, 8, (80, 80, 80)))
-group2.add(Point(630, 440, 8, (80, 80, 80)))
-group2.add(Point(480, 200, 8, (80, 80, 80)))
-# Lines
-group2.add(Triangle(group2.sprites[1], group2.sprites[2], group2.sprites[3], 3, "magenta", "white"))
-# Text
-group2.add(Text("A", "white", group2.sprites[1], 20))
-group2.add(Text("B", "white", group2.sprites[2], 20))
-group2.add(Text("C", "white", group2.sprites[3], 20))
-
-##########################
-# GROUP 3
-##########################
-
-# Background
-group3.add(BGText("Quadratic Bézier Curve", (120, 120, 120), (screen_width/2, 50), 32))
-
-group3.add(Point(280, 340, 8, "white"))
-group3.add(Point(700, 250, 8, "white"))
-group3.add(Point(480, 100, 8, "orange"))
-group3.add(Point(500, 420, 8, "white"))
-group3.add(Point(800, 330, 8, "orange"))
-
-group3.add(Line(group3.sprites[1], group3.sprites[3], 1, "cyan"))
-group3.add(Line(group3.sprites[2], group3.sprites[3], 1, "cyan"))
-group3.add(Line(group3.sprites[2], group3.sprites[5], 1, "green"))
-group3.add(Line(group3.sprites[4], group3.sprites[5], 1, "green"))
-
-# Curve
-group3.add(QuadBezierCurve(group3.sprites[1], group3.sprites[2], group3.sprites[3], "yellow", 3))
-group3.add(QuadBezierCurve(group3.sprites[2], group3.sprites[4], group3.sprites[5], "red", 3))
-
-group3.add(Text("A", "white" , group3.sprites[1], 20))
-group3.add(Text("B", "white" , group3.sprites[2], 20))
-group3.add(Text("C", "orange", group3.sprites[3], 20))
-group3.add(Text("D", "white" , group3.sprites[4], 20))
-group3.add(Text("E", "orange", group3.sprites[5], 20))
 
 ##########################
 # GROUPS
 ##########################
-groups = iter([group1, group2, group3])
-main = next(groups)
+groups = load("config.txt")
+idx = 0
+main = groups[idx]
 
 while running:
     screen.fill(bg_col)
@@ -263,10 +334,9 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-            try:
-                main = next(groups)
-            except StopIteration:
-                pass
+            idx += 1
+            if idx == len(groups): idx = 0
+            main = groups[idx]
     
     main.update()
     main.draw(screen)
